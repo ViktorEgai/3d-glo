f564559439aff83a3967da5ac57e46e1f7e0806e
Egai Viktor <egai.viktor94@gmail.com>
2021-08-19 18:59:27 +0300

    добавил анимацию для popup

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* eslint-disable indent */
 window.addEventListener('DOMContentLoaded', () => {
-	// Таймер 
+	// Таймер
 	function countTimer(deadline) {
 		const timerHours = document.querySelector('#timer-hours'),
 			timerMinutes = document.querySelector('#timer-minutes'),
@@ -16,7 +16,6 @@ window.addEventListener('DOMContentLoaded', () => {
 					if (hours < 10) hours = '0' + hours;
 					if (minutes < 10) minutes = '0' + minutes;
 					if (seconds < 10) seconds = '0' + seconds;
-					
 			return { hours, minutes, seconds, timeRemaining };
 		}
 		function uploadTime() {	
@@ -30,8 +29,7 @@ window.addEventListener('DOMContentLoaded', () => {
 				timerSeconds.textContent = '00';
 			}
 		}
-		setInterval(uploadTime,1000);		
-		
+		setInterval(uploadTime, 1000);
 	}
 	countTimer('22 aug 2021');
 
@@ -48,24 +46,35 @@ window.addEventListener('DOMContentLoaded', () => {
 
 		menuBtn.addEventListener('click', handlerMenu);
 		closeBtn.addEventListener('click', handlerMenu);
-		menuItem.forEach(item => item.addEventListener('click', handlerMenu));	
+		menuItem.forEach(item => item.addEventListener('click', handlerMenu));
 	}
 	toggleMenu();
 
-	// открытие popup окна 
+	// открытие popup окна
 	function openPopup() {
 		const popupBtn = document.querySelectorAll('.popup-btn'),
 			popup = document.querySelector('.popup'),
-			popupClose = document.querySelector('.popup-close');
-
-		function showPopup() {
-
-		}
+			popupClose = document.querySelector('.popup-close'),
+			popupContent = document.querySelector('.popup-content');
+		let count = -300;
 
 		popupBtn.forEach(btn => {
-			btn.addEventListener('click', () => popup.style.display = 'block');
+			btn.addEventListener('click', () => {
+				popup.style.display = 'block';
+				// анимация popup
+				function showPopup() {
+					console.log(count);
+					count += 7;
+					popupContent.style.top = count + 'px';
+					if (count <= 100) setTimeout(showPopup, 1);
+				}
+				if (window.screen.width > 768) showPopup();
+			});
 		});
-		popupClose.addEventListener('click', () => popup.style.display = 'none');
+		popupClose.addEventListener('click', () => {
+			popup.style.display = 'none';
+			count = -300;
+		});
 	}
 	openPopup();
 });
