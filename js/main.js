7b4768afa57c12af69ff8bf23824cb59afb4f8fe
Egai Viktor <egai.viktor94@gmail.com>
2021-08-19 17:45:15 +0300

    Сделал кнопку меню активной

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* eslint-disable indent */
-
 window.addEventListener('DOMContentLoaded', () => {
+	// Таймер 
 	function countTimer(deadline) {
 		const timerHours = document.querySelector('#timer-hours'),
 			timerMinutes = document.querySelector('#timer-minutes'),
@@ -16,22 +16,56 @@ window.addEventListener('DOMContentLoaded', () => {
 					if (hours < 10) hours = '0' + hours;
 					if (minutes < 10) minutes = '0' + minutes;
 					if (seconds < 10) seconds = '0' + seconds;
+					
 			return { hours, minutes, seconds, timeRemaining };
 		}
-
-		function uploadTime() {
+		function uploadTime() {	
 			if (getTimeRemaining().timeRemaining > 0) {
 				timerHours.textContent = getTimeRemaining().hours;
 				timerMinutes.textContent = getTimeRemaining().minutes;
 				timerSeconds.textContent = getTimeRemaining().seconds;
-				setInterval(uploadTime, 1000);
 			} else {
 				timerHours.textContent = '00';
 				timerMinutes.textContent = '00';
 				timerSeconds.textContent = '00';
 			}
 		}
-		uploadTime();
+		setInterval(uploadTime,1000);		
+		
 	}
-	countTimer('19 aug 2021');
+	countTimer('22 aug 2021');
+
+	// открытие меню
+	function toggleMenu() {
+		const menuBtn = document.querySelector('.menu'),
+			menu = document.querySelector('menu'),
+			closeBtn = document.querySelector('.close-btn'),
+			menuItem = menu.querySelectorAll('li');
+
+		function handlerMenu() {
+			menu.classList.toggle('active-menu');
+		}
+
+		menuBtn.addEventListener('click', handlerMenu);
+		closeBtn.addEventListener('click', handlerMenu);
+		menuItem.forEach(item => item.addEventListener('click', handlerMenu));	
+	}
+	toggleMenu();
+
+	// открытие popup окна 
+	function openPopup() {
+		const popupBtn = document.querySelectorAll('.popup-btn'),
+			popup = document.querySelector('.popup'),
+			popupClose = document.querySelector('.popup-close');
+
+		function showPopup() {
+
+		}
+
+		popupBtn.forEach(btn => {
+			btn.addEventListener('click', () => popup.style.display = 'block');
+		});
+		popupClose.addEventListener('click', () => popup.style.display = 'none');
+	}
+	openPopup();
 });
